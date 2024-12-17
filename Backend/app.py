8fe3a3a8708038b0ee9c02b63656a6606d90a055
captainALI21 <162017013+captainALI21@users.noreply.github.com>
2024-12-17 08:28:32 +0500

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -8,8 +8,7 @@ app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'MYSQLPASSWORD'
-app.config['MYSQL_DATABASE'] = 'questionanswerplatform'
+
 
 mysql.init_app(app)
 
